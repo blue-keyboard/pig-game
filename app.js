1899a24d3b751c5f2e81f5a18f0d43d94e7bb1c3
Daniel Rivas <48565203+blue-keyboard@users.noreply.github.com>
2020-06-25 19:08:05 +0200

    fix minor error

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-*/
-
-// * setter
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML =   '<em>' + dice + '</em>';
-
-// * getter
-// let x = document.querySelector('#score-1').textContent;
-// console.log(x)
-
-// * get element by its id
-// document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
-
-// * Syntax to change CSS properties
-// document.querySelector('.dice').style.display = 'none';
 
 let scores, roundScore, activePlayer, gamePlaying, lastDice, gameMode, maxScore;
 
@@ -62,7 +39,7 @@ document.getElementById('btn-roll').addEventListener('click', function() {
         document.querySelector('.dice1').src = 'dice-' + dice1 + '.png';
         document.querySelector('.dice2').src = 'dice-' + dice2 + '.png';
 
-        if (dice1 !== 1 && dice2 !== 2) {
+        if (dice1 !== 1 && dice2 !== 1) {
             roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
